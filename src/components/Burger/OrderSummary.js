b82a1974e1ed1f0e3887f5d299247e5117e751aa
Noah Pope <nlpope@tutanota.com>
2020-04-08 10:10:10 -0700

    fixed modal button display on mobile

--- src/components/Burger/OrderSummary.js
+++ src/components/Burger/OrderSummary.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import Button from "../UI/Button";
+import classes from "../../css/OrderSummary.module.css";
 
 //changing to class to add lifecycle hook/method
 //this is b/c we only want this to re-render if it's visible
@@ -21,19 +22,23 @@ class OrderSummary extends Component {
     );
     return (
       <>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>{ingredientSummary}</ul>
-        <p>
-          <strong>total Price: ${this.props.price.toFixed(2)}</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button btnType={"Danger"} clicked={this.props.purchaseCancelled}>
-          CANCEL
-        </Button>
-        <Button btnType={"Success"} clicked={this.props.purchaseContinued}>
-          CONTINUE
-        </Button>
+        <div className={classes.OrderSummary}>
+          <h3>Your Order</h3>
+          <p>A delicious burger with the following ingredients:</p>
+          <ul>{ingredientSummary}</ul>
+          <p>
+            <strong>total Price: ${this.props.price.toFixed(2)}</strong>
+          </p>
+          <p>Continue to Checkout?</p>
+          <div className={classes.btn}>
+            <Button btnType={"Danger"} clicked={this.props.purchaseCancelled}>
+              CANCEL
+            </Button>
+            <Button btnType={"Success"} clicked={this.props.purchaseContinued}>
+              CONTINUE
+            </Button>
+          </div>
+        </div>
       </>
     );
   }
