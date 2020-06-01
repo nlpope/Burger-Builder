e1dc7c58d5179b2af5b4bfa65de03508a127f1d7
Noah Pope <nlpope@tutanota.com>
2020-06-01 12:11:18 -0700

    setting up query params

--- src/components/Order/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary.js
@@ -1,30 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 
-// import Burger from "../Burger/Burger";
+import Burger from "../Burger/Burger";
 import Button from "../UI/Button";
 import classes from "../../css/CheckoutSummary.module.css";
 
-class CheckoutSummary extends Component {
-  state = {};
-  render() {
-    console.log(this.props.ingredients);
-
-    return (
-      <div className={classes.CheckoutSummary}>
-        <h1>We hope it tastes great!</h1>
-        {/* review vid 221: building checkout */}
-        {/* <div style={{ width: "100%", margin: "auto" }}>
+const CheckoutSummary = (props) => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>We hope it tastes great!</h1>
+      {/* review vid 221: building checkout */}
+      {/* <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredents={props.ingredients} />
       </div> */}
-        <Button btnType="Danger" clicked={this.props.returnToBuild}>
-          CANCEL
-        </Button>
-        <Button btnType="Success" clicked={this.props.returnToBuild}>
-          CONTINUE
-        </Button>
-      </div>
-    );
-  }
-}
+      <Button btnType="Danger" clicked={props.checkoutCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.checkoutContinued}>
+        CONTINUE
+      </Button>
+    </div>
+  );
+};
 
 export default CheckoutSummary;
--- src/containers/Checkout.js
+++ src/containers/Checkout.js
@@ -13,9 +13,28 @@ class Checkout extends Component {
       meat: 1,
     },
   };
-  returnToBuild() {
-    this.props.history.goBack();
+
+  componentDidMount() {
+    //new function
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      // ['salad', '1']
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients: ingredients });
   }
+  // returnToBuild() {
+  //   this.props.history.goBack();
+  // }
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
 
   render() {
     return (
@@ -26,8 +45,10 @@ class Checkout extends Component {
           component={() => CheckoutSummary(this.state.ingredients)}
         /> */}
         <CheckoutSummary
-          returnToBuild={() => this.returnToBuild()}
+          // returnToBuild={() => this.returnToBuild()}
           ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
         />
       </div>
     );
